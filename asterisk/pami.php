49546fe073453ba3ff2d5b9098daf9c5eaab6ebe
Nikolai Bulashev <nvbulashev@gmail.com>
2020-12-05 13:42:06 +0300

    unanswered version

--- asterisk/pami.php
+++ asterisk/pami.php
@@ -3,9 +3,17 @@ require 'vendor/autoload.php';
 require 'utils.php';
 require 'config.php';
 
+const DEBUG = TRUE;
+
+date_default_timezone_set('Europe/Moscow');
+
 use PAMI\Client\Impl\ClientImpl as PamiClient;
 use PAMI\Message\Event\EventMessage;
 use PAMI\Listener\IEventListener;
+use PAMI\Message\Action\GetVarAction;
+use PAMI\Message\Action;
+use PAMI\Message\Action\BridgeInfoAction;
+
 $options = array(
     'host' => '127.0.0.1',
     'scheme' => 'tcp://',
@@ -15,14 +23,128 @@ $options = array(
     'connect_timeout' => 10,
     'read_timeout' => 10
 );
+$uniqIdArr = array();
 $pamiClient = new PamiClient($options);
 // Open the connection
+// ($callid, $direction, $src, $dst, $started, $answered, $completed, $recording, $host)
 $pamiClient->open();
-$pamiClient->registerEventListener(function (EventMessage $event) {
-    if($event->getName() == 'BridgeEnter'){
-      var_dump($event);
-    }
-});
+$pamiClient->registerEventListener(function (EventMessage $event) use ($pamiClient, $host) {
+	  global $asthost;
+	  global $uniqIdArr;
+      if ($event instanceof PAMI\Message\Event\BridgeEnterEvent or $event instanceof PAMI\Message\Event\BridgeLeaveEvent) {
+		if ($event instanceof PAMI\Message\Event\BridgeEnterEvent) {
+			$started = date("Y-m-d H:i:s");
+			$duration = 0;
+			$finished = '';
+			if ($event->getBridgeNumChannels() == 2) {
+				$answered = date("Y-m-d H:i:s");
+				$vars = $event->getAllChannelVariables()[strtolower($event->GetChannel())];
+				var_dump($vars);
+				if (strlen(trim($vars['realcalleridnum'])) == 3) {
+					$cid1 = trim($vars['realcalleridnum']);
+					$cid2 = trim($vars['dial_number']);
+					$direction = 'outgoing';
+				} elseif (strlen(trim(($event->getConnectedLineNum()))) == 3)  {
+					$cid1 = trim($event->getCallerIDName());
+					$cid2 = trim($event->getConnectedLineNum());
+					$direction = 'incoming';
+				}	
+				$rec_file = end(explode('/',$vars['mixmonitor_filename']));
+				$uniqueid = $event->getUniqueid();
+				$rec_link = $asthost."/".date("Y")."/".date("m")."/".date("d").'/'.$rec_file;
+				
+				if (($cid1 && $cid2) and ((strlen($cid1) > 3) or (strlen($cid2) > 3)) and ((strlen($cid1) < 10 ) or (strlen($cid2) < 10)) ) {
+				  $response = sendApiRequest($uniqueid, $direction, $cid1, $cid2, '', $answered, '', $rec_link, $host );
+				  echo "request_type:".$event->getName().PHP_EOL;;
+				  echo "sending request $uniqueid, $direction, $cid1, $cid2, $started, $answered, $finished, $rec_link, $host".PHP_EOL;;
+				  echo "response $response".PHP_EOL; 
+				}
+			}
+		} else {
+			$started = '';
+			$finished = date("Y-m-d H:i:s");
+			$duration = 0;
+			if ($event->getBridgeNumChannels() <= 1) {
+				$vars = $event->getAllChannelVariables()[strtolower($event->GetChannel())];
+				if (strlen($vars['realcalleridnum']) > 0 ) {
+					var_dump($vars);
+					if (strlen(trim($vars['realcalleridnum'])) == 3) {
+						$cid1 = trim($vars['realcalleridnum']);
+						$cid2 = trim($vars['dial_number']);
+						$direction = 'outgoing';
+					} elseif (strlen(trim($event->getConnectedLineNum())) == 3 ) {
+						$cid1 = trim($event->getCallerIDName());
+						$cid2 = trim($event->getConnectedLineNum());
+						$direction = 'incoming';
+					}	
+					$rec_file = end(explode('/',$vars['mixmonitor_filename']));
+					$uniqueid = $event->getUniqueid();
+					$rec_link = $asthost."/".date("Y")."/".date("m")."/".date("d").'/'.$rec_file;
+					
+					if (($cid1 && $cid2) and ((strlen($cid1) > 3) or (strlen($cid2) > 3)) and ((strlen($cid1) < 10 ) or (strlen($cid2) < 10)) ) {
+					  $response = sendApiRequest($uniqueid, $direction, $cid1, $cid2, '' , '' , $finished, $rec_link, $host );
+					  echo "request_type:".$event->getName().PHP_EOL;;
+					  echo "sending request $uniqueid, $direction, $cid1, $cid2, '' , '', $finished, $rec_link, $host".PHP_EOL;;
+					  echo "response $response".PHP_EOL;
+					}
+				}
+			}
+		}
+		var_dump($event);
+	  }
+	  else if ($event instanceof PAMI\Message\Event\NewstateEvent or $event instanceof PAMI\Message\Event\NewextenEvent) {
+		if (trim($event->getKey('ChannelStateDesc')) == 'Ring') {
+			$started = date("Y-m-d H:i:s");
+			$uniqueid = $event->getKey('Uniqueid');
+			if (strlen($event->getKey('CallerIDNum')) == 3 ) { //incoming ring
+				$cid1 = preg_replace('/[^0-9.]+/', '', $event->getKey('CallerIDNum'));
+				$cid2 = preg_replace('/[^0-9.]+/', '', $event->getKey('Exten'));
+				$direction = 'outgoing';
+			} else {
+				$cid1 = preg_replace('/[^0-9.]+/', '', $event->getKey('CallerIDNum'));
+				$cid2 = preg_replace('/[^0-9.]+/', '', $event->getKey('Exten'));
+				$direction = 'incoming';
+			}
+			if (($cid1 && $cid2) and ((strlen($cid1) > 3) or (strlen($cid2) > 3)) and ((strlen($cid1) < 10 ) or (strlen($cid2) < 10)) and strlen($cid1) > 1 and strlen($cid2) > 1 ) {
+				echo strlen($cid1)." ".strlen($cid2).PHP_EOL;
+				if (array_search($uniqueid, $uniqIdArr) === false) {
+					echo "firstly meet $uniqueid".PHP_EOL;
+					$response = sendApiRequest($uniqueid, $direction, $cid1, $cid2, $started, '', '', $rec_link, $host );
+					echo "request_type:".$event->getName().PHP_EOL;;
+					echo "sending request $uniqueid, $direction, $cid1, $cid2, $started, '', '', '', $host".PHP_EOL;;
+					echo "response $response".PHP_EOL;
+					array_unshift($uniqIdArr, $uniqueid);
+					$uniqIdArr = array_slice($uniqIdArr, 0, 50);
+				} else {
+					echo "already saw $uniqueid ... skipping".PHP_EOL;
+				}
+			}
+			var_dump($event);
+		} else {
+			var_dump($event);
+		}
+	  }	else if ($event instanceof PAMI\Message\Event\NewchannelEvent) {
+        $cid1 = $event->getCallerIDNum();
+        $context = $event->getContext();
+        $exten = $event->getExtension();
+        $uniqueid = $event->getUniqueID();
+        $direction = 'incoming';
+        $started = date("Y-m-d H:i:s");
+        $answered = NULL;
+        $finished = NULL;
+        if ((strlen($cid1) >= 6) && ($context == 'from-pstn') && (strlen($exten) == 4) ) {
+          $response = sendApiRequest($uniqueid, $direction, $cid1, $cid2, $started, $answered, $finished, $rec_link, $host );
+          echo "request_type:".$event->getName().PHP_EOL;;
+          echo "sending request $uniqueid, $direction, $cid1, $cid2, $started, $answered, $finished, $rec_link, $host".PHP_EOL;;
+          echo "response $response".PHP_EOL;
+        }
+        var_dump($event);
+      } else {
+		if (! $event instanceof PAMI\Message\Event\VarSetEvent) {
+			var_dump($event);
+		}
+      }
+  });
 $running = true;
 // Main loop
 while($running) {
--- asterisk/utils.php
+++ asterisk/utils.php
@@ -1,38 +1,74 @@
-<?php function sendApiRequest ($callid, $direction, $src, $dst, $started, $duration, $status, $recording, $host) {
+<?php
+const DEBUG = TRUE;
+
+function sendApiRequest ($callid, $direction, $src, $dst, $started, $answered, $completed, $recording, $host) {
   $callid = str_replace('.','',$callid);
   //smarttek
   //test
   //$host = 'https://app.salesap.ru/telephony_external/61237144-ee01-4e45-bb20-60aad2ff2129';
   $ch = curl_init();
-  $completed = DateTime::createFromFormat('Y-m-d H:i:s',$started)->add(new DateInterval('PT'.$duration.'S'))->format(DateTime::ISO8601);
-  $started = DateTime::createFromFormat('Y-m-d H:i:s',$started)->format(DateTime::ISO8601);
+  //$completed = DateTime::createFromFormat('Y-m-d H:i:s',$started)->add(new DateInterval('PT'.$duration.'S'))->format(DateTime::ISO8601);
   $fields = array (
     'call_id' => $callid,
     'direction' => $direction,
-    'src_phone_number' => $src,
-    'dst_phone_number' => $dst,
-    'started_at' => $started,
-    'completed_at' => $completed,
-    'recording' => $recording,
+    'src_phone_number' => normPhone($src),
+    'dst_phone_number' => normPhone($dst),
   );
-  if ($status == 'NO ANSWER') {$fields['status'] = 'no_answer'; }
+  if ($recording) {
+    $fields['recording'] = $recording;
+  }
+  if ($started) {
+    $started = DateTime::createFromFormat('Y-m-d H:i:s',$started)->format(DateTime::ISO8601);
+    $fields['started_at'] = $started;
+  }
+  if ($answered) {
+    $answered = DateTime::createFromFormat('Y-m-d H:i:s',$answered)->format(DateTime::ISO8601);
+    $fields['answered_at'] = $answered;
+  }
+  if ($completed) {
+    $completed = DateTime::createFromFormat('Y-m-d H:i:s',$completed)->format(DateTime::ISO8601);
+    $fields['completed_at'] = $completed;
+  }
+  // if ($status) {
+      // $fields['status'] = $status;
+  // }
+  //if ($status == 'NO ANSWER') {$fields['status'] = 'no_answer'; }
   $fields_string = http_build_query($fields);
   curl_setopt($ch, CURLOPT_URL,"$host");
   curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_SSLVERSION, 6);
   curl_setopt($ch, CURLOPT_POSTFIELDS,$fields_string);
   if (DEBUG) echo PHP_EOL;var_dump($fields_string);echo PHP_EOL;
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $server_output = curl_exec ($ch);
   $json = json_decode($server_output);
-  if (DEBUG) echo 'RESPONSE'.var_dump($json); echo PHP_EOL;
+  echo 'RESPONSE'.var_dump($json); echo PHP_EOL;
   curl_close ($ch);
-  if (isset($json->id)) {
-    if (DEBUG) echo 'SUCCESS'; echo PHP_EOL;
-    return TRUE;
-  } else {
-    if (DEBUG) echo 'ERROR';echo PHP_EOL;
-    return FALSE;
+  if (isset($json->status)) {
+      if ($json->status == "success") {
+          if (DEBUG) echo 'SUCCESS'; echo PHP_EOL;
+          return TRUE;
+        } else {
+          if (DEBUG) echo 'ERROR';echo PHP_EOL;
+          return FALSE;
+        }
   }
 }
+
+function normPhone($phone) {
+  $resPhone = preg_replace("/[^0-9]/", "", $phone);
+
+  if (strlen($resPhone) === 6) {
+    $resPhone = '+74712'.$resPhone;
+  }
+  if (strlen($resPhone) === 10) {
+    $resPhone = '+7'.$resPhone;
+  }
+  if (strlen($resPhone) === 11) {
+    $resPhone[0] = preg_replace("/^8/", "7", $resPhone);
+  }
+  return $resPhone;
+}
+
 
 ?>
